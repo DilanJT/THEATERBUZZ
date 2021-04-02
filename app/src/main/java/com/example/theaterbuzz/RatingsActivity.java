0230d889d75804d12e205e231e3404ae866e9e43
DilanJT <dicmandilan@gmail.com>
2021-04-03 05:04:37 +0530

    register movie acitivity views initialized

--- app/src/main/java/com/example/theaterbuzz/RatingsActivity.java
+++ app/src/main/java/com/example/theaterbuzz/RatingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.theaterbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,14 @@ public class RatingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ratings);
+
+        // customizing the tool bar which was manually setup in the xml file
+        Toolbar toolbar = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            toolbar = (Toolbar) findViewById(R.id.toolbar2);
+            toolbar.setTitle(getString(R.string.register_movie));
+            toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+            setSupportActionBar(toolbar);
+        }
     }
 }
--- app/src/main/java/com/example/theaterbuzz/RegisterMovieActivity.java
+++ app/src/main/java/com/example/theaterbuzz/RegisterMovieActivity.java
@@ -1,14 +1,47 @@
 package com.example.theaterbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterMovieActivity extends AppCompatActivity {
+
+    EditText title;
+    EditText year;
+    EditText director;
+    EditText actors;
+    EditText rating;
+    EditText review;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_movie);
+
+        // customizing the tool bar which was manually setup in the xml file
+        Toolbar toolbar = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            toolbar = (Toolbar) findViewById(R.id.toolbar2);
+            toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+            setSupportActionBar(toolbar);
+        }
+
+        //initializing the views
+        title = (EditText) findViewById(R.id.registerTitle);
+        year = (EditText) findViewById(R.id.registerYear);
+        director = (EditText) findViewById(R.id.registerDirector);
+        actors = (EditText) findViewById(R.id.registerActors);
+        rating = (EditText) findViewById(R.id.registerRating);
+        review = (EditText) findViewById(R.id.registerreview);
+        btnSave = (Button) findViewById(R.id.btnRegisterSave);
+    }
+
+    // function to check user have input text in the correct way
+    public void checkTextBoxValidity(){
+
     }
 }
