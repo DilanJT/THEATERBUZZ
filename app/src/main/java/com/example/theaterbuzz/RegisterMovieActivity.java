e6464fe14e6a48d748e8c622f499d1c5da42d5f5
DilanJT <dicmandilan@gmail.com>
2021-04-09 08:51:09 +0530

    year text box min max filter completed

--- app/src/main/java/com/example/theaterbuzz/RegisterMovieActivity.java
+++ app/src/main/java/com/example/theaterbuzz/RegisterMovieActivity.java
@@ -12,10 +12,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.theaterbuzz.model.MinMaxFilter;
+import com.example.theaterbuzz.model.Movie;
+import com.example.theaterbuzz.model.MovieDataHelper;
 
 import java.util.Date;
 
-public class RegisterMovieActivity extends AppCompatActivity {
+public class RegisterMovieActivity extends AppCompatActivity{
 
     EditText title;
     EditText year;
@@ -24,6 +26,10 @@ public class RegisterMovieActivity extends AppCompatActivity {
     EditText rating;
     EditText review;
     Button btnSave;
+
+    String yearText;
+    int minYear = 1895;
+//    String[] yearTextArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +44,7 @@ public class RegisterMovieActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
 
-        //initializing the views
+        //initializing the viewsOn
         title = (EditText) findViewById(R.id.registerTitle);
         year = (EditText) findViewById(R.id.registerYear);
         director = (EditText) findViewById(R.id.registerDirector);
@@ -52,10 +58,36 @@ public class RegisterMovieActivity extends AppCompatActivity {
 
         // setting the year filter to allow only to inputs year greater than 1895
         Date d = new Date();
-        int currentYear = d.getYear();
-        currentYear = currentYear + 1900;
+        int cYear = d.getYear(); // getting the current date as the max value filter.
+        int currentYear = cYear + 1900;
         Log.d("YEAR", "Current year is " + currentYear);
-        year.setFilters(new InputFilter[] { new MinMaxFilter(1895, currentYear)});
+        //year.setFilters(new InputFilter[] { new MinMaxFilter("1895", Integer.toString(currentYear))});
+
+        // There is a problem when setting a bigger amount as the min filter
+        // so have to use onFocusChangeListener to check the min value
+        // problem was found and understood using the below link,
+        // https://stackoverflow.com/questions/14212518/is-there-a-way-to-define-a-min-and-max-value-for-edittext-in-android/28743065#28743065
+        year.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(!year.getText().toString().isEmpty()){
+                        if(Integer.parseInt(year.getText().toString()) < minYear){
+                            Toast.makeText(RegisterMovieActivity.this, "Year should be greater than or equal 1895", Toast.LENGTH_SHORT).show();
+                            year.setText(null);
+                        }
+                    }
+
+                    if(!year.getText().toString().isEmpty()) {
+                        if(Integer.parseInt(year.getText().toString()) > currentYear){
+                            Toast.makeText(RegisterMovieActivity.this, "Year should be less than or equal" + currentYear, Toast.LENGTH_SHORT).show();
+                            year.setText(null);
+                        }
+                    }
+                }
+            }
+        });
+
     }
 
     // function to check user have input text in the correct way
@@ -71,11 +103,25 @@ public class RegisterMovieActivity extends AppCompatActivity {
             !rating.getText().toString().isEmpty() &&
             !review.getText().toString().isEmpty()){
 
+//            if(Integer.parseInt(year.getText().toString()) > )
+
             // save the details to the database
 
-
+            MovieDataHelper db = new MovieDataHelper(this);
+            db.addMovie(new Movie(
+                    title.getText().toString(),
+                    Integer.parseInt(year.getText().toString()),
+                    director.getText().toString(),
+                    actors.getText().toString(),
+                    Integer.parseInt(rating.getText().toString()),
+                    review.getText().toString(),
+                    false // setting the default value of the isMovie false.
+            ));
+            Toast.makeText(this, "Movie " + title.getText().toString() + " successfully added", Toast.LENGTH_SHORT).show();
         }else {
             Toast.makeText(this, "Not saved! Please fill all the details", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
