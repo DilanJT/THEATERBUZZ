e5fde82f434e8051f03d5c0bd0de03df855a3f96
DilanJT <dicmandilan@gmail.com>
2021-04-11 06:22:45 +0530

    started the search screen

--- app/src/main/java/com/example/theaterbuzz/SearchActivity.java
+++ app/src/main/java/com/example/theaterbuzz/SearchActivity.java
@@ -1,14 +1,36 @@
 package com.example.theaterbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText searchBox;
+    Button btnLookup;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // customizing the tool bar which was manually setup in the xml file
+        Toolbar toolbar = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            toolbar = (Toolbar) findViewById(R.id.toolbar6);
+
+            toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+            setSupportActionBar(toolbar);
+        }
+
+        // initializing views
+        searchBox = (EditText) findViewById(R.id.searchBox);
+        btnLookup = (Button) findViewById(R.id.btnLookup);
+        listView = (ListView) findViewById(R.id.searchListView);
     }
 }
